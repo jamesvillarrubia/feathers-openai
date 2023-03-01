4d03594c5a6b06b2f4bd818edb1ac87fa2aef442
James Villarrubia <james.villarrubia@gmail.com>
2023-03-01 17:43:04 -0500

    ci: fixes the release-it

--- .release-it.js
+++ .release-it.js
@@ -1,6 +1,6 @@
-module.export = {
+module.exports = {
   "git":{
-    "requireCleanWorkingDir": false,
+    "requireCleanWorkingDir": false
   },
   "github": {
     "release": true
@@ -10,8 +10,47 @@ module.export = {
   },
   "plugins": {
     "@release-it/conventional-changelog": {
+      "whatBump": (commits,options)=>{
+          let defaults = {
+            build: 'ignore',
+            ci: 'ignore',
+            docs: 'ignore',
+            feat: 'minor',
+            fix: 'patch',
+            perf: 'patch',
+            refactor: 'ignore',
+            test: 'ignore'
+          }
+          let types = (options?.preset?.types || [])
+          .reduce((a, v) => {
+            return { ...a, [v.type]: v.release}
+          }, {}) 
+
+          types = Object.assign({},defaults,types)
+          let breakings = 0
+          let features = 0
+          let levelSet = ['major','minor','patch','ignore']
+          let level = Math.min.apply(Math, commits.map(commit => {
+            let level = levelSet.indexOf(types[commit.type])
+            level = level<0?3:level
+            if (commit.notes.length > 0) {
+              breakings += commit.notes.length
+            }
+            if(commit.type === 'feat'){
+              features += 1;
+            }
+            return level
+          }))
+      
+          return {
+            level: level,
+            reason: breakings === 1
+              ? `There is ${breakings} BREAKING CHANGE and ${features} features`
+              : `There are ${breakings} BREAKING CHANGES and ${features} features`
+          }
+      },
       "preset": {
-        "name": "conventionalcommits",
+        "name": "angular",
         "types": [
           {
             "type": "refactor",
@@ -31,13 +70,9 @@ module.export = {
           },
           {
             "type": "ci",
-            "release": "minor"
+            "release": "patch"
           }
-        ],
-        // "whatBump": ()=>{
-        //   return null
-        //   // null
-        // }
+        ]
       }
     }
   }
